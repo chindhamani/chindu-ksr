e3fe4743605324ecec3b1589ac9e8b4785ad17f2
chindhamani <chindhamani1997@gmail.com>
2017-07-06 14:54:24 +0530

    Update and rename alphabet to alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Alphabet
+{
+  
+  public static void main(String args[])
+  {
+    
+    Scanner s=new Scanner(System.in);
+    char a;
+   a = s.next().charAt(0);
+    if(a>='a'&&a<='z')
+    
+    {   
+      
+      System.out.println(a+"alphabet");
+      }
+      
+      }
+      }
